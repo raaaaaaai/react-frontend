e8fe1bd367ad2993f692f2f3b45fc09a68733b93
Rai Muhammad Awais <96009190+raaaaaaai@users.noreply.github.com>
2023-08-25 18:10:42 +0500

    Update Contact.js

--- frontend/src/containers/Contact.js
+++ frontend/src/containers/Contact.js
@@ -37,7 +37,7 @@ const Contact = ({ setAlert }) => {
         setLoading(true);
         axios
             .post(
-                `${process.env.REACT_APP_API_URL}/api/contacts/`,
+                `/api/contacts/`,
                 { name, email, subject, message },
                 config
             )
